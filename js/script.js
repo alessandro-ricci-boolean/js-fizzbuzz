cdefd52d7cd70aa0c7344cff93bb94407739273d
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-22 23:37:04 +0100

    added FizzBuzz feature

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ for(let i=1; i<101; i++){
     if(i%3 == 0){
         console.log(i+" Fizz");
     }
-    if(i%5 == 0){
+    else if(i%5 == 0){
         console.log(i+" Buzz");
+    }
+    else if(i%15 == 0){
+        console.log(i+" FizzBuzz");
     }
     else{
         console.log(i);
